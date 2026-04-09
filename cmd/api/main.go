9850e114b859999d672b576ae3f95653a76d7b9d
Tahir <tahirqasimov016@gmail.com>
2026-04-09 21:01:17 +0400

    working students,deparment,classses without relations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"my-project/internal/classes"
 	"my-project/internal/departments"
 	"my-project/internal/students"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 type Server struct {
 	studentHandler    *students.Handler
 	departmentHandler *departments.Handler
+	classHandler      *classes.Handler
 }
 
 func DepedencyInjection() *Server {
@@ -18,14 +20,20 @@ func DepedencyInjection() *Server {
 	departmentService := departments.NewService(departmentRepo)
 	departmentHanler := departments.NewHandler(departmentService)
 
+	//Classes domain
+	classRepo := classes.NewMemoryRepo()
+	classService := classes.NewService(classRepo)
+	classHandler := classes.NewHanler(classService)
+
 	//Students domain
 	studentRepo := students.NewMemoryRepo()
-	studentService := students.NewService(studentRepo)
+	studentService := students.NewService(studentRepo, departmentRepo)
 	studentHandler := students.NewHandler(studentService)
 
 	return &Server{
 		studentHandler:    studentHandler,
 		departmentHandler: departmentHanler,
+		classHandler:      classHandler,
 	}
 }
 
@@ -34,7 +42,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	//Students raotes
+	//Students rautes
 	mux.HandleFunc("GET /students/{id}", server.studentHandler.GetByID)
 	mux.HandleFunc("GET /students", server.studentHandler.GetAll)
 	mux.HandleFunc("POST /students", server.studentHandler.Craete)
@@ -43,6 +51,11 @@ func main() {
 	mux.HandleFunc("GET /departments/{id}", server.departmentHandler.GetDepartment)
 	mux.HandleFunc("GET /departments", server.departmentHandler.GetDepartments)
 	mux.HandleFunc("POST /departments", server.departmentHandler.Create)
+
+	//Class rautes
+	mux.HandleFunc("GET /classes/{id}", server.classHandler.GetClassByID)
+	mux.HandleFunc("GET /classes", server.classHandler.GetAllClasses)
+	mux.HandleFunc("POST /classes", server.classHandler.CreateClass)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
