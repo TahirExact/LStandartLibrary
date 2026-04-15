ab29db5bf7557efd9c61ebe17a685e332c0773b1
Tahir <tahirqasimov016@gmail.com>
2026-04-15 22:01:36 +0400

    Added new model

--- internal/models/school.go
+++ internal/models/school.go
@@ -9,6 +9,13 @@ type Student struct {
 	ID           int    `json:"id"`
 	Name         string `json:"name"`
 	DepartmentID int    `json:"department_id"`
+	Classes      []*Class 
+}
+
+type StudentClasses struct {
+	ID        int `json:"id"`
+	StudentID int `json:"student_id"`
+	ClassID   int `json:"class_id"`
 }
 
 type Class struct {
